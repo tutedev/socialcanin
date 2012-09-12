980cf38e14b818dacebe2fd165a3d6b76debe855
matias <matias@matias-laptop.(none)>
2012-09-12 11:49:40 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialcanin::Application.config.secret_token = 'ce2b21cab8782ef471d3fbaed969a2342b9caae314426e88ed98279b811149d28daaf60c11aec6685d72a72ddb1c52bc6eb480a2c450ab266b2368ff6da6237f'
